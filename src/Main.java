6b263ae8beba1841dcaed059b156364f42da71ac
mehdinoori20 <mehdi.noori1994@hotmail.com>
2023-12-12 13:39:16 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        DBConnection dataSource = new DBConnection();
-        dataSource.open();
 
-        System.out.println(dataSource.queryCategories());
-        ArrayList<Category> categories = dataSource.queryCategories();
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.open();
+
+        System.out.println(dbConnection.queryCategories().size());
+
+        ArrayList<Category> categories = dbConnection.queryCategories();
+
         for (Category katt: categories) {
             System.out.println(katt.toString());
         }
-        dataSource.createTable();
+
+        dbConnection.updateCategory("Sprit", 1);
     }
+
 }
